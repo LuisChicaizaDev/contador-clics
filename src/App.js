bfbf30dbdab0abd589f0f3c6fec6ab8158a01558
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2024-07-18 11:28:51 +0200

    feat: logo luis

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+//importamos el logo
+import logoLuis from './img/logo-luis-developer.png'
 
 function App() {
   return (
     <div className="App">
-     
+      <div className='logo-contenedor'>
+        <img 
+          className='logo-luis'
+          src={logoLuis}
+          alt='Logo de Luis'
+        />
+      </div>
+
     </div>
   );
 }
