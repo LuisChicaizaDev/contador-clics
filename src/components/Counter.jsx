16c2762d9c447639bdc66594aee8262da4fc6cb5
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2025-05-27 17:13:19 +0200

    update: counter y button componentes de clase

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import '../stylesheets/counter.css';
 
+/**
+ * Creamos un componente de clase
+ * Usamos 'this.props' para acceder a los props del objeto de propiedades
+ * del Componente
+ */
 
-//Creamos el componente funcional
-function Counter({ numClicks }){
-    return(
-        <div className='counter'>
-            {numClicks}
-        </div>
-    );
+class Counter extends React.Component {
+    render(){
+        return(
+            <div className='counter'>
+                {this.props.numClicks}
+            </div>
+        );
+    }
 }
 
 export default Counter;
